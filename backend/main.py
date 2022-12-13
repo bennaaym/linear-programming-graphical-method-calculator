af0924522e52a801fbee4ee2b8c4385cf1fe9a4a
bennaaym <mr.aymen.bennabi@gmail.com>
2022-12-14 00:25:37 +0300

    :adhesive_bandage: fix cors error

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from solver import solve_lpp
 from model.lpp_model import LPP
 
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Endpoint
 @app.post("/")
